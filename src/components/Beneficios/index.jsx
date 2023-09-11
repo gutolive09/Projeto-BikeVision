b39ae517b6a522d577fd3686733889603123bc28
gutolive09 <gutolive01@gmail.com>
2023-09-11 11:12:00 -0300

    Componente beneficios adicionado.

---
+++ src/components/Beneficios/index.jsx
@@ -0,0 +1,42 @@
+// import React from 'react'
+
+export default function Beneficios() {
+  return (
+    <>
+      <section>
+        <div>
+          <h2>Beneficios</h2>
+          <div className="beneficios-container">
+            <div className="beneficios-card">
+              <h3>Praticidade</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                voluptas debitis iste dolorem. Error accusamus cumque voluptas
+                recusandae. Saepe labore veritatis voluptatibus praesentium enim
+                earum, voluptates commodi harum distinctio corrupti.
+              </p>
+            </div>
+            <div className="beneficios-card">
+              <h3>Flexibilidade</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reiciendis nostrum, debitis odio incidunt tempore nulla sapiente
+                libero id at error sequi exercitationem neque rem voluptas
+                repellat, harum ullam culpa numquam.
+              </p>
+            </div>
+            <div className="beneficios-card">
+              <h3>Escalabilidade</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aliquam, esse ducimus aperiam enim sit veritatis mollitia magnam
+                temporibus officiis delectus accusamus consequuntur explicabo
+                nesciunt minus voluptates nihil maxime, porro vero.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "../../components/Carousel";
 import "./Home.css";
+import Beneficios from "../../components/Beneficios";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <main>
         <h1>Bem vindo ao futuro do seguro de bicicletas</h1>
         <Carousel />
+        <Beneficios/>
       </main>
     </>
   );
