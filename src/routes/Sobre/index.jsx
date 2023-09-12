d8598acae36df2c68162d9ef1f8825d12d704b5f
gutolive09 <gutolive01@gmail.com>
2023-09-12 19:40:07 -0300

    Rota SobreNos criada.

--- src/routes/Sobre/index.jsx
+++ src/routes/Sobre/index.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import "./Sobre.css"
 
 export default function Sobre() {
   return (
-    <div>Sobre nós</div>
+    <>
+      <section className='equipe'>
+        <h2>A Equipe</h2>
+        <div className='container-membros'>
+          <div>
+            <h3>André Rohregger Machado</h3>
+            <p>RM:98110</p>
+          </div>
+          <div>
+            <h3>Caio da Silva Martins</h3>
+            <p>RM:550563</p>
+          </div>
+          <div>
+            <h3>Gabriel Siqueira Rodrigues</h3>
+            <p>RM:98626</p>
+          </div>
+          <div>
+            <h3>Gustavo de Oliveira Azevedo</h3>
+            <p>RM:550548</p>
+          </div>
+          <div>
+            <h3>Mateus Mantovani Araújo</h3>
+            <p>RM:98524</p>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
