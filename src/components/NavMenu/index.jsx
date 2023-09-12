bdda3615d2a9557fecbba63967a2d4b856ce04af
gutolive09 <gutolive01@gmail.com>
2023-09-12 11:47:52 -0300

    Links adicionados ao header.

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -1,14 +1,35 @@
 import React from "react";
-import './NavMenu.css';
+import "./NavMenu.css";
+import { Link } from "react-router-dom";
 
 export default function NavMenu(props) {
   return (
     <>
       <nav>
-        <ul style={{flexDirection: props.direction}}>
-          <li style={{fontSize: props.fontSize}}>Home</li>
-          <li style={{fontSize: props.fontSize}}>Vistoria</li>
-          <li style={{fontSize: props.fontSize}}>Sobre nós</li>
+        <ul style={{ flexDirection: props.direction }}>
+          <li>
+            <Link>
+              <p className="item" style={{ fontSize: props.fontSize }}>
+                Home
+              </p>
+            </Link>
+          </li>
+
+          <li style={{ fontSize: props.fontSize }}>
+            <Link>
+              <p className="item" style={{ fontSize: props.fontSize }}>
+                Vistoria
+              </p>
+            </Link>
+          </li>
+
+          <li style={{ fontSize: props.fontSize }}>
+            <Link>
+              <p className="item" style={{ fontSize: props.fontSize }}>
+                Sobre nós
+              </p>
+            </Link>
+          </li>
         </ul>
       </nav>
     </>
