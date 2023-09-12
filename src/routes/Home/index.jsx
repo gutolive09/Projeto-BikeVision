5604a8745ec4c0c5af2b6df7e8964952287ded5b
gutolive09 <gutolive01@gmail.com>
2023-09-12 09:20:45 -0300

    Banner e descrição reestruturados.

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,15 +1,23 @@
-import React from "react";
-import Carousel from "../../components/Carousel";
 import "./Home.css";
 import Beneficios from "../../components/Beneficios";
+import Banner from "../../components/Banner";
+import {BsBicycle} from 'react-icons/bs'
 
 export default function Home() {
   return (
     <>
       <main>
-        <h1>Bem vindo ao futuro do seguro de bicicletas</h1>
-        <Carousel />
-        <Beneficios/>
+        <Banner/>
+        <section className="desc">
+          <h2>A solução para o seu negocio de seguros</h2>
+          <BsBicycle className="bike"/>
+          <hr/>
+          <p>
+            Atualmente é necessario a presença de uma pessoa para realizar o processo de vistoria de bicicletas, o que deixa o processo custoso para a seguradora e inconveniente para o cliente.
+            É por isso que a BikeVision surgiu, para automatizar o processo e aprimorar a experiência da seguradora e do cliente.
+          </p>
+        </section>
+        <Beneficios />
       </main>
     </>
   );
